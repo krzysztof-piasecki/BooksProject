6efa206e917391228d2aa10e61cc3fc7aae670e5
Krzysztof <krzysztof.piasecki1410@gmail.com>
2020-12-05 20:19:25 +0100

    Added javadoc to APIs

--- src/main/java/pl/krzysztof/piasecki/homework/api/BooksController.java
+++ src/main/java/pl/krzysztof/piasecki/homework/api/BooksController.java
@@ -1,4 +1,4 @@
-package pl.krzysztof.piasecki.homework.controller;
+package pl.krzysztof.piasecki.homework.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +17,11 @@ public class BooksController {
     @Autowired
     BookService bookService;
 
+    /**
+     * To retrive data from specific ISBN the path is "isbn:{id}"
+     * @param id
+     * @return
+     */
     @GetMapping(path = "isbn:{id}")
     public Book getBooksByIsbn(@PathVariable String id) {
         Book book = bookService.getBookByIsbn(id);
@@ -27,6 +32,12 @@ public class BooksController {
         }
         return book;
     }
+
+    /**
+     * To retrive the list of books by category the path is "category:{category}"
+     * @param category
+     * @return
+     */
 
     @GetMapping(path = "category:{category}")
     public List<Book> getBooksByCategory(@PathVariable String category) {
@@ -39,6 +50,10 @@ public class BooksController {
         return booksByCategory;
     }
 
+    /**
+     * To retrive authors by average books written rating the path is "authors_rating"
+     * @return
+     */
     @GetMapping(path = "authors_rating")
     public List<AuthorRating> getBooksByAuthorRatings() {
         List<AuthorRating> bookList = bookService.getAuthorsAverageRatings();
@@ -50,6 +65,11 @@ public class BooksController {
         return bookList;
     }
 
+    /**
+     * To retrive book for the maximum pages the path is "pages:{pages}"
+     * @param pages
+     * @return
+     */
     @GetMapping(path = "pages:{pages}")
     public Book getBookByPages(@PathVariable Integer pages) {
         Book book = bookService.getBookByPageNumber(pages);
@@ -61,6 +81,13 @@ public class BooksController {
         return book;
     }
 
+    /**
+     * To retrive data for list of book by the user reading speed and daily hours for reading
+     * the path is best/pace:{pages}/daily:{hours}
+     * @param pages
+     * @param hours
+     * @return
+     */
     @GetMapping(path = "best/pace:{pages}/daily:{hours}")
     public List<Book> getBooksByReadingSkills(@PathVariable Integer pages, @PathVariable Integer hours) {
         List<Book> bookList = bookService.getBooksByReadingSkills(pages, hours);
--- src/main/java/pl/krzysztof/piasecki/homework/reader/JsonReader.java
+++ src/main/java/pl/krzysztof/piasecki/homework/reader/JsonReader.java
@@ -3,8 +3,6 @@ package pl.krzysztof.piasecki.homework.reader;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 public interface JsonReader {
     JSONObject getData();
